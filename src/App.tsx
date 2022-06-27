f68815928dcc8a53b7370fe42d41f5ba82e5ac49
Bui The Anh <bui.the.anh@sun-asterisk.com>
2022-06-27 17:06:27 +0700

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p data-testid="app-title">GITHUB_ACTION_1</p> <p>GITHUB_ACTION_1</p>
+        <p data-testid="app-title">GITHUB_ACTION</p> <p>GITHUB_ACTION_1</p>
       </header>
     </div>
   );
